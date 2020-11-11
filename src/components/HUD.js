f05ad94fab5be75bc48721cc8c8a41c52c6e2663
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-11-11 13:39:35 -0500

    added expand / collapse behavior to HUD in main view

--- src/components/HUD.js
+++ src/components/HUD.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './HUD.css';
 
 function HUD() {
+
+  let [ pressCount, setPressCount ] = useState(0);
+
+  const adjustHUD = () => {
+    let styleAdjust
+
+    // let offset = "";
+    // let adjustHUD = document.getElementById("HUD").style.bottom = `${offset}`;
+
+
+    if (pressCount === 0) {
+      document.getElementById("HUD").style.bottom = "-67vh";
+    } else if (pressCount === 1) {
+      document.getElementById("HUD").style.bottom = "-33vh";
+    } else if (pressCount === 2) {
+      document.getElementById("HUD").style.bottom = "-6vh";
+    } else {
+      document.getElementById("HUD").style.bottom = "-95vh";
+      setPressCount(pressCount = -1);
+    }
+
+    setPressCount(pressCount + 1);
+
+  }
+
   return(
-    <section className="HUD">
-      <h2>HUD</h2>
+    <section id="HUD" className="HUD">
+      <button className="HUD__pill-button" onClick={adjustHUD} />
     </section>
   );
 }
