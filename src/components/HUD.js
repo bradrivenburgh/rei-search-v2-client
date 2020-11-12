4454a07edcd51826758fa8e36576a362d5b73ae3
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-11-12 11:22:16 -0500

    experimenting with HUD

--- src/components/HUD.js
+++ src/components/HUD.js
@@ -2,31 +2,113 @@ import React, { useState } from 'react';
 import './HUD.css';
 
 function HUD() {
-
   let [ pressCount, setPressCount ] = useState(0);
 
-  const adjustHUD = () => {
-    let offset;
+  // const adjustHUDHeight = () => {
+  //   let offset;
 
+  //   if (pressCount === 0) {
+  //     offset = "-33vh";
+  //   } else if (pressCount === 1) {
+  //     offset = "-6vh";
+  //   } else if (pressCount === 2) {
+  //     offset = "-91vh";
+  //   } else {
+  //     offset = "-67vh";
+  //   }
+  //   document.getElementById("HUD").style.bottom = `${offset}`;
+    
+  //   pressCount <= 2
+  //   ? setPressCount(pressCount + 1)
+  //   : setPressCount(pressCount = 0);
+  // }
+
+  const adjustHUDHeight = () => {
+    let containerHeight, contentHeight;
     if (pressCount === 0) {
-      offset = "-33vh";
+      containerHeight = "67%";
     } else if (pressCount === 1) {
-      offset = "-6vh";
+      containerHeight = "94%";
     } else if (pressCount === 2) {
-      offset = "-95vh";
+      containerHeight = "12%";
     } else {
-      offset = "-67vh";
+      containerHeight = "33%";
     }
-    document.getElementById("HUD").style.bottom = `${offset}`;
+    document.getElementById("HUD").style.height = `${containerHeight}`;
     
     pressCount <= 2
     ? setPressCount(pressCount + 1)
     : setPressCount(pressCount = 0);
+
   }
+
+
+  const openCity = (e, cityName) => {
+    let tabContent = document.getElementsByClassName("HUD__tab__tabcontent");
+    for (let i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    
+    let tabLinks = document.getElementsByClassName("HUD__tab__tablinks");
+    for (let i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    e.target.className += " active";
+
+    // Get the element with id="defaultOpen" and click on it
+    // document.getElementById("defaultOpen").click();
+  }
+  
 
   return(
     <section id="HUD" className="HUD">
-      <button className="HUD__pill-button" onClick={adjustHUD} />
+      <button className="HUD__pill-button" onClick={adjustHUDHeight} />
+      
+      <div className="HUD__tab">
+        <button id="defaultOpen" className="HUD__tab__tablinks" onClick={(e) => openCity(e, 'economics')}>economics</button>
+        <button className="HUD__tab__tablinks" onClick={(e) => openCity(e, 'demographics')}>demographics</button>
+        <button className="HUD__tab__tablinks" onClick={(e) => openCity(e, 'properties')}>properties</button>
+      </div>
+
+      <div id="economics" className="HUD__tab__tabcontent">
+          <h3>London</h3>
+          <p>London is the capital city of England.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>Dolore adipisicing voluptate excepteur culpa.</p>
+          <p>This is the last line</p>
+      </div>
+
+      <div id="demographics" className="HUD__tab__tabcontent">
+        <h3>Paris</h3>
+        <p>Paris is the capital of France.</p>
+      </div>
+
+      <div id="properties" className="HUD__tab__tabcontent">
+        <h3>Tokyo</h3>
+        <p>Tokyo is the capital of Japan.</p>
+      </div>
     </section>
   );
 }
