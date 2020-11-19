3e46d891933d322e05d60f3b45d6cbc88eb13774
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-11-19 11:54:41 -0500

    modified Autosuggest to be dropdown on focus and with a scrollable list

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,89 +1,122 @@
 import React from 'react';
 import Autosuggest from 'react-autosuggest';
+import './Search.css';
 
 const cities = [
-  {name: 'Philadelphia, PA'},  
-  {name: 'Moorestown, NJ'},
+  {
+    name: 'Philadelphia, PA',
+  },
+  {
+    name: 'Moorestown, NJ',
+  },
+  {
+    name: 'Langhorne, PA',
+  },
+  {
+    name: 'Cinnaminson, NJ',
+  },
+  {
+    name: 'Elm',
+  },
+  {
+    name: 'Go',
+  },
+  {
+    name: 'Haskell',
+  },
+  {
+    name: 'Java',
+  },
+  {
+    name: 'Javascript',
+  },
+  {
+    name: 'Perl',
+  },
+  {
+    name: 'PHP',
+  },
+  {
+    name: 'Python',
+  },
+  {
+    name: 'Ruby',
+  },
+  {
+    name: 'Scala',
+  }
 ];
- 
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = value => {
-  const inputValue = value.trim().toLowerCase();
-  const inputLength = inputValue.length;
- 
-  return inputLength === 0 ? [] : cities.filter(lang =>
-    lang.name.toLowerCase().slice(0, inputLength) === inputValue
+
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return cities.filter(language => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function shouldRenderSuggestions() {
+  return true;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
   );
-};
- 
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
- 
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
-  <div>
-    {suggestion.name}
-  </div>
-);
+}
 
 class Search extends React.Component {
   constructor() {
     super();
- 
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
+
     this.state = {
       value: '',
       suggestions: []
     };
   }
- 
-  onChange = (event, { newValue }) => {
+
+  onChange = (event, { newValue, method }) => {
     this.setState({
       value: newValue
     });
   };
- 
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
+  
   onSuggestionsFetchRequested = ({ value }) => {
     this.setState({
       suggestions: getSuggestions(value)
     });
   };
- 
-  // Autosuggest will call this function every time you need to clear suggestions.
+
   onSuggestionsClearRequested = () => {
     this.setState({
       suggestions: []
     });
   };
- 
+
   render() {
     const { value, suggestions } = this.state;
- 
-    // Autosuggest will pass through all these props to the input.
     const inputProps = {
-      placeholder: 'Type a city name',
+      placeholder: "Type 'c'",
       value,
       onChange: this.onChange
     };
- 
-    // Finally, render it!
+
     return (
       <Autosuggest
         suggestions={suggestions}
         onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
         onSuggestionsClearRequested={this.onSuggestionsClearRequested}
         getSuggestionValue={getSuggestionValue}
+        shouldRenderSuggestions={shouldRenderSuggestions}
         renderSuggestion={renderSuggestion}
-        inputProps={inputProps}
-      />
+        inputProps={inputProps} />
     );
   }
 }
