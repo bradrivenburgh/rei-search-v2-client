50a1fe34f297b106bb3d30a74614f84f3f68d074
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-02 11:18:45 -0500

    modified Nav.test.js

--- src/components/Nav.test.js
+++ src/components/Nav.test.js
@@ -1,7 +1,19 @@
+import React, {useState} from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './Nav';
 
+function NavTestComponent() {
+  /* Menu State */
+  let [menuOffset, setMenuOffset] = useState("-250px");
+
+  return(
+    <BrowserRouter>
+      <Nav setMenuOffset={ setMenuOffset } />
+    </BrowserRouter>
+  );
+}
+
 test('renders Nav', () => {
-  render(<BrowserRouter><Nav /></BrowserRouter>);
+  render(<NavTestComponent />);
 });
