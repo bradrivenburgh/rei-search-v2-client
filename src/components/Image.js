5c948e92bfeb00e1c587ed3b43eb8005f35265cc
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-07 14:52:25 -0500

    modified error handling for bad photo urls phase 2

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,35 +1,38 @@
 import React, {useState} from 'react';
 import house from '../images/default-house.png';
 
-function Image({photo, alt}) {
-      // try fetch call to see if there is a successful response and replace
-      // src with the value of data-src
-      // https://stackoverflow.com/questions/47263773/add-image-data-to-html-file
- 
+function Image({ photo, alt }) {
+  // try fetch call to see if there is a successful response and replace
+  // src with the value of data-src
+  // https://stackoverflow.com/questions/47263773/add-image-data-to-html-file
+
   let [isValid, setIsValid] = useState(false);
 
   /**
    * Replaces images that aren't retrievable with a default photo
-   * @param {array} photos
+   * @param {string} photos
    */
   const addSafeSrc = (url) => {
     fetch(url)
-    .then(response => {
-      if (response.ok) {
-        setIsValid(true);
-        return;
-      }
-      throw new Error('The image url was invalid');
-    })
-    .catch(e => {
-      console.error(e)
-    });
+      .then((response) => {
+        if (response.ok) {
+          setIsValid(true);
+          return;
+        }
+        throw new Error("The image url was invalid");
+      })
+      .catch((e) => {
+        console.error(e);
+      });
     return isValid;
-}
+  };
+
   return (
     <img
-      className="property-image"
-      src={addSafeSrc(photo) ? photo : house} 
+      className='property-image'
+      height={125}
+      width={200}
+      src={addSafeSrc(photo) ? photo : house}
       alt={alt}
     />
   );
@@ -37,7 +40,7 @@ function Image({photo, alt}) {
 
 Image.defaultProps = {
   photos: [],
-  alt: ''
-}
+  alt: "",
+};
 
 export default Image;
--- src/components/carousel/Slide.js
+++ src/components/carousel/Slide.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import house from '../../images/default-house.png';
 
 function Slide({photos, activeIndex}) {
+  let [isValid, setIsValid] = useState(false);
+
   /**
    * Replaces images that aren't retrievable with a default photo
-   * @param {string} photo
+   * @param {string} url
    */
-  const addSafeSrc = (photoUrl) => {
-      try {
-        return photoUrl;
-      } catch(e) {
-        return house;
-      }
+  const addSafeSrc = (url) => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          setIsValid(true);
+          return;
+        }
+        throw new Error("The image url was invalid");
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+    return isValid;
   };
 
   return (
     <div>
       {
-        photos.map((photoUrl, index) => 
+        photos.map((url, index) => 
         <div className={
           index === activeIndex ? 'active-slide' : 'inactive-slide'}
           key={index}>
-            <img src={addSafeSrc(photoUrl)} alt={`slide ${index}`} />
+            <img 
+            src={addSafeSrc(url) ? url : house}
+            alt={`slide ${index}`} 
+            />
         </div>
         )
       }
