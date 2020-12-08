cbcaafddc58fbed57b684a71a8befa12c1abd8ca
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-07 21:25:39 -0500

    made optimizations to HUD

--- src/components/HUD.js
+++ src/components/HUD.js
@@ -17,22 +17,13 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
     setHUDPosition,
     setActiveTab,
     HUDScrollTops,
-    setHUDScrollTops
+    setHUDScrollTops,
   } = HUDState;
 
   /* Refs for HUD tab content */
   let economicsTabContent = useRef(null);
   let demographicsTabContent = useRef(null);
   let propertiesTabContent = useRef(null);
-
-  /**
-   * Open a tab after a search is made
-   */
-  useEffect(() => {
-    if (defaultTab) {
-      handleOpenTab('econTab');
-    }
-  });
 
   /**
    * Receives event object from onClick listener attached to
@@ -56,7 +47,7 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
     },
     [setHUDScrollTops]
   );
-  
+
   /**
    * Adds an event listener to the entire mainView listening for clicks
    * Need to narrow it to just act on clicks to the menu
@@ -64,19 +55,19 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
   useEffect(() => {
     if (ref && ref.current) {
       let mainViewNode = ref.current;
-      mainViewNode.addEventListener('click', handleScrollPosition, false);
+      mainViewNode.addEventListener("click", handleScrollPosition, false);
       return () => {
-        mainViewNode.removeEventListener('click', handleScrollPosition, false);
-      }
+        mainViewNode.removeEventListener("click", handleScrollPosition, false);
+      };
     }
-  }, [handleScrollPosition, ref])
+  }, [handleScrollPosition, ref]);
 
   /* Sets the scroll position for the tabs  */
   useEffect(() => {
     economicsTabContent.current.scrollTop = HUDScrollTops.econTab;
     demographicsTabContent.current.scrollTop = HUDScrollTops.demogTab;
     propertiesTabContent.current.scrollTop = HUDScrollTops.propsTab;
-  }, [HUDScrollTops])
+  }, [HUDScrollTops]);
 
   /* FUNCTIONS FOR HUD BEHAVIOR */
 
@@ -98,27 +89,30 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
    * @param {number} pressCount
    * @param {object} allHUDHeights
    */
-  const handleHUDHeight = (pressCount) => {
-    // HUD to baseScreen
-    if (pressCount > 3) {
-      pressCount = 0;
-    }
-    // HUD to fullScreen
-    else if (pressCount < 0) {
-      pressCount = 3;
-    }
-    setPressCount(pressCount);
+  const handleHUDHeight = useCallback(
+    (pressCount) => {
+      // HUD to baseScreen
+      if (pressCount > 3) {
+        pressCount = 0;
+      }
+      // HUD to fullScreen
+      else if (pressCount < 0) {
+        pressCount = 3;
+      }
+      setPressCount(pressCount);
 
-    // Set HUDPosition; pressCount === 0 || 1 || 2 || 3
-    // corresponding to ["69px","33vh","67vh","100vh"]
-    setHUDPosition(Object.values(HUDHeights())[pressCount]);
+      // Set HUDPosition; pressCount === 0 || 1 || 2 || 3
+      // corresponding to ["69px","33vh","67vh","100vh"]
+      setHUDPosition(Object.values(HUDHeights())[pressCount]);
 
-    // If no active tab, set it to 'econTab'
-    let { econTab, demogTab, propsTab } = activeTab;
-    if (!econTab && !demogTab && !propsTab && pressCount > 0) {
-      setActiveTab(changeActiveValue("econTab"));
-    }
-  };
+      // If no active tab, set it to 'econTab'
+      let { econTab, demogTab, propsTab } = activeTab;
+      if (!econTab && !demogTab && !propsTab && pressCount > 0) {
+        setActiveTab(changeActiveValue("econTab"));
+      }
+    },
+    [activeTab, setActiveTab, setHUDPosition, setPressCount]
+  );
 
   /**
    * Helper function used to reduce repetition when calling
@@ -135,9 +129,9 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
     const tabRefDictionary = {
       econTab: economicsTabContent.current,
       demogTab: demographicsTabContent.current,
-      propsTab: propertiesTabContent.current
-    };  
-  
+      propsTab: propertiesTabContent.current,
+    };
+
     tabs[activeTab] = tabRefDictionary[activeTab];
     return tabs;
   };
@@ -147,23 +141,33 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
    * adjustHeight() if the HUD is collapsed in order to show content.
    * @param {string} selectedTab
    */
-  const handleOpenTab = (selectedTab = "econTab") => {
-    // Expand HUD if tab is clicked when just tabs are showing
-    if (pressCount === 0) {
-      handleHUDHeight(pressCount + 1);
-    }
-
-    // Check if there is already an active tab when the
-    // defaultTab request is made
-    if (defaultTab) {
-      if (activeTab.demogTab || activeTab.propsTab) {
-        return;
+  const handleOpenTab = useCallback(
+    (selectedTab = "econTab") => {
+      // Expand HUD if tab is clicked when just tabs are showing
+      if (pressCount === 0) {
+        handleHUDHeight(pressCount + 1);
       }
-    }
+      // Check if there is already an active tab when the
+      // defaultTab request is made
+      if (defaultTab) {
+        if (activeTab.demogTab || activeTab.propsTab) {
+          return;
+        }
+      }
+      // Set active tab
+      setActiveTab(changeActiveValue(selectedTab));
+    },
+    [activeTab, defaultTab, pressCount, setActiveTab, handleHUDHeight]
+  );
 
-    // Set active tab
-    setActiveTab(changeActiveValue(selectedTab));
-  };
+  /**
+   * Open a tab after a search is made
+   */
+  useEffect(() => {
+    if (defaultTab) {
+      handleOpenTab("econTab");
+    }
+  }, [defaultTab, handleOpenTab]);
 
   return (
     <section id='HUD' className='HUD' style={{ height: HUDPosition }}>
@@ -189,7 +193,7 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
                 : "HUD__tab__tablinks"
             }
             onClick={() => handleOpenTab("econTab")}>
-              <EconomicsIcon />
+            <EconomicsIcon />
           </button>
           <button
             id='demographics-btn'
@@ -199,7 +203,7 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
                 : "HUD__tab__tablinks"
             }
             onClick={() => handleOpenTab("demogTab")}>
-             <DemographicsIcon />
+            <DemographicsIcon />
           </button>
           <button
             id='properties-btn'
@@ -209,7 +213,7 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
                 : "HUD__tab__tablinks"
             }
             onClick={() => handleOpenTab("propsTab")}>
-             <PropertiesIcon />
+            <PropertiesIcon />
           </button>
         </div>
       </div>
@@ -217,27 +221,30 @@ const HUD = React.forwardRef(({defaultTab, HUDState}, ref) => {
       <div className='HUD__tabcontent__container'>
         <div
           ref={economicsTabContent}
-          className={activeTab.econTab
-            ? 'HUD__tabcontent HUD__tabcontent--visible'
-            : 'HUD__tabcontent HUD__tabcontent--hidden'
+          className={
+            activeTab.econTab
+              ? "HUD__tabcontent HUD__tabcontent--visible"
+              : "HUD__tabcontent HUD__tabcontent--hidden"
           }>
           <StatsTabs id='economics' />
         </div>
 
         <div
           ref={demographicsTabContent}
-          className={activeTab.demogTab
-            ? 'HUD__tabcontent HUD__tabcontent--visible'
-            : 'HUD__tabcontent HUD__tabcontent--hidden'
+          className={
+            activeTab.demogTab
+              ? "HUD__tabcontent HUD__tabcontent--visible"
+              : "HUD__tabcontent HUD__tabcontent--hidden"
           }>
           <StatsTabs id='demographics' />
         </div>
 
         <div
           ref={propertiesTabContent}
-          className={activeTab.propsTab
-            ? 'HUD__tabcontent HUD__tabcontent--visible'
-            : 'HUD__tabcontent HUD__tabcontent--hidden'
+          className={
+            activeTab.propsTab
+              ? "HUD__tabcontent HUD__tabcontent--visible"
+              : "HUD__tabcontent HUD__tabcontent--hidden"
           }>
           <PropertiesTab />
         </div>
