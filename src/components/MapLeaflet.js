fb1acd02fdccee28037f905a9e9d882121f99c53
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-10 13:53:15 -0500

    removed useMap hook from imports

--- src/components/MapLeaflet.js
+++ src/components/MapLeaflet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from 'react-leaflet';
 import config from '../config';
 import './MapLeaflet.css';
 
