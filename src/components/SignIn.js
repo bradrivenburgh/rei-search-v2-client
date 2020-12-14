d962994f209b32115bfd4d6074738bd37a3eff3e
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-14 10:41:32 -0500

    added form state to SignIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './SignIn.css';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./SignIn.css";
 
 function SignIn() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
   let history = useHistory();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    history.push('/')
-  }
+    history.push("/");
+  };
 
   return (
     <section className='sign-in'>
@@ -21,18 +31,28 @@ function SignIn() {
 
       <form action='#' onSubmit={(e) => handleSubmit(e)}>
         <label htmlFor='email'>Email:</label>
-        <input type='text' id='email' name='email' />
+        <input
+          type='text'
+          id='email'
+          name='email'
+          value={formData.email}
+          onChange={(e) => handleChange(e)}
+        />
 
         <label htmlFor='password'>Password:</label>
-        <input type='text' id='password' name='password' />
+        <input
+          type='text'
+          id='password'
+          name='password'
+          value={formData.password}
+          onChange={(e) => handleChange(e)}
+        />
 
         <div className='sign-in__buttons'>
-          <button type="button" onClick={() => history.push('/')}>
-              Cancel
+          <button type='button' onClick={() => history.push("/")}>
+            Cancel
           </button>
-          <button>
-              Submit
-          </button>
+          <button>Submit</button>
         </div>
       </form>
     </section>
