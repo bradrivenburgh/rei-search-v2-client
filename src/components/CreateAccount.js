479d22e9d9bcb3d7317440588ae34f315fe6e3b4
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-14 10:27:15 -0500

    added form state to CreateAccount

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './CreateAccount.css';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./CreateAccount.css";
 
 function CreateAccount() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
   let history = useHistory();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    history.push('/')
-  }
+    history.push("/");
+  };
 
   return (
     <section className='create-account'>
@@ -20,28 +33,56 @@ function CreateAccount() {
       </header>
 
       <form action='#' onSubmit={(e) => handleSubmit(e)}>
-        <label htmlFor='first-name'>First Name:</label>
-        <input type='text' id='first-name' name='first-name' />
+        <label htmlFor='firstName'>First Name:</label>
+        <input
+          type='text'
+          id='firstName'
+          name='firstName'
+          value={formData.firstName}
+          onChange={(e) => handleChange(e)}
+        />
 
-        <label htmlFor='last-name'>Last Name:</label>
-        <input type='text' id='last-name' name='last-name' />
+        <label htmlFor='lastName'>Last Name:</label>
+        <input
+          type='text'
+          id='lastName'
+          name='lastName'
+          value={formData.lastName}
+          onChange={(e) => handleChange(e)}
+        />
 
         <label htmlFor='email'>Email:</label>
-        <input type='text' id='email' name='email' />
+        <input
+          type='text'
+          id='email'
+          name='email'
+          value={formData.emailName}
+          onChange={(e) => handleChange(e)}
+        />
 
         <label htmlFor='password'>Password:</label>
-        <input type='text' id='password' name='password' />
+        <input
+          type='text'
+          id='password'
+          name='password'
+          value={formData.password}
+          onChange={(e) => handleChange(e)}
+        />
 
-        <label htmlFor='verify-password'>Repeat Password:</label>
-        <input type='text' id='verify-password' name='verify-password' />
+        <label htmlFor='repeatPassword'>Repeat Password:</label>
+        <input
+          type='text'
+          id='repeatPassword'
+          name='repeatPassword'
+          value={formData.repeatPassword}
+          onChange={(e) => handleChange(e)}
+        />
 
         <div className='create-account__buttons'>
-          <button type="button" onClick={() => history.push('/')}>
-              Cancel
+          <button type='button' onClick={() => history.push("/")}>
+            Cancel
           </button>
-          <button>
-              Submit
-          </button>
+          <button>Submit</button>
         </div>
       </form>
     </section>
