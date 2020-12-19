19c7e686b3caf05317dfd73f85ff6f9ca68f7e47
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-12-19 13:53:52 -0500

    fixed more tests

--- src/components/StatsTabs.test.js
+++ src/components/StatsTabs.test.js
@@ -1,35 +1,9 @@
-import React, {useState} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import { render } from '@testing-library/react';
-import {Context} from '../Context';
-import {fakeStats} from '../mockData';
 import StatsTabs from './StatsTabs';
 
-const id = "economics";
-
-function StatsTabsTestWrapper({id}) {
-  /* Data from API */
-  let [statistics, setStatistics] = useState(fakeStats);
-  let searchResults = {
-    statistics,
-    setStatistics,
-  };
-  const contextValues = {
-    searchResults,
-  };
-
-  return(
-  <Context.Provider value={contextValues}>
-    <BrowserRouter>
-      <StatsTabs id={id} />
-    </BrowserRouter>
-  </Context.Provider>
-  );
-}
-
-
 describe('StatsTabs', () => {
-  test("renders StatsTabs", () => {
-    render(<StatsTabsTestWrapper id={id} />);
+  test("renders to the DOM", () => {
+    render(<BrowserRouter><StatsTabs /></BrowserRouter>);
   });
 });
