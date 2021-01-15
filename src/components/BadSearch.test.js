1d06805d7576b882417d0939a010c89595e1009f
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-01-15 09:58:20 -0500

    fixed BadSearch.test.js

--- src/components/BadSearch.test.js
+++ src/components/BadSearch.test.js
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import About from './About';
+import BadSearch from './BadSearch';
 
-describe('About', () => {
+describe('Bad Search', () => {
   test('renders to the DOM', () => {
-    render(<BrowserRouter><About /></BrowserRouter>)
+    render(<BrowserRouter><BadSearch /></BrowserRouter>)
   });
 });
