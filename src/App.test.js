3d2d2df40e0d57e808b6ce158dbd3ae5d2c52f32
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-03-25 13:57:02 -0400

    added tests for About modal in App.test.js and About.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe('App', () => {
-  test('renders to the DOM', () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
-  });  
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+describe("App", () => {
+  test("renders to the DOM", () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  });
+
+  test("About modal closes after clicking 'X' button", () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    const closeButton = screen.getByRole("button", { name: "X" });
+    userEvent.click(closeButton);
+
+    const hiddenCloseButton = screen.queryByRole("button", { name: "X" });
+    expect(hiddenCloseButton).not.toBeInTheDocument();
+  });
 });
--- src/components/About.test.js
+++ src/components/About.test.js
@@ -1,9 +1,49 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import About from './About';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+import About from "./About";
 
-describe('About', () => {
-  test('renders to the DOM', () => {
-    render(<BrowserRouter><About /></BrowserRouter>)
+describe("About", () => {
+  test("renders to the DOM", () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>
+    );
+  });
+
+  test("displays about modal on first visit", () => {
+    render(
+      <BrowserRouter>
+        <About
+          aboutState={{ visited: "false", handleAddAboutVisited: jest.fn() }}
+        />
+      </BrowserRouter>
+    );
+
+    const pageTitle = screen.getByRole("heading", { name: "rei-search" });
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+  test("function to close modal is called after clicking 'X' button", async () => {
+    let visited = "false";
+    const closeModal = jest.fn(() => {
+      visited = "true";
+    });
+
+    render(
+      <BrowserRouter>
+        <About
+          aboutState={{ visited: visited, handleAddAboutVisited: closeModal }}
+        />
+      </BrowserRouter>
+    );
+    
+    const closeButton = screen.getByRole("button", { name: "X" });
+    userEvent.click(closeButton);
+    
+    // check to make sure handler that hides the About modal has been called 
+    expect(closeModal).toHaveBeenCalledTimes(1);
+    expect(visited).toBe("true");
   });
 });
