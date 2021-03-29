a3cd30f06d6b82189ca27070f7414e7109a81177
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-03-29 10:24:58 -0400

    added test for Carousel.test.js

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,35 +1,31 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import LeftArrow from './LeftArrow';
-import RightArrow from './RightArrow';
-import Slide from './Slide';
-import './Carousel.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import LeftArrow from "./LeftArrow";
+import RightArrow from "./RightArrow";
+import Slide from "./Slide";
+import "./Carousel.css";
 
-function Carousel({photos}) {
+function Carousel({ photos }) {
   let [activeIndex, setActiveIndex] = useState(0);
 
   const handleNextSlide = () => {
     let index = activeIndex;
     let length = photos.length;
-    index === length - 1
-      ? index = 0
-      : index++
-      setActiveIndex(index)
+    index === length - 1 ? (index = 0) : index++;
+    setActiveIndex(index);
   };
 
   const handlePrevSlide = () => {
     let index = activeIndex;
     let length = photos.length;
-    index < 1
-      ? index+= length - 1
-      : index--;
-    setActiveIndex(index)
+    index < 1 ? (index += length - 1) : index--;
+    setActiveIndex(index);
   };
 
   return (
     <section className='carousel'>
       <Slide photos={photos} activeIndex={activeIndex} />
-      <div className="carousel__button-container">
+      <div className='carousel__button-container'>
         <LeftArrow onPrevSlide={handlePrevSlide} />
         <RightArrow onNextSlide={handleNextSlide} />
       </div>
@@ -38,12 +34,11 @@ function Carousel({photos}) {
 }
 
 Carousel.defaultProps = {
-  photos: []
-}
+  photos: [],
+};
 
 Carousel.propTypes = {
-  photos: PropTypes.array.isRequired
-  
-}
+  photos: PropTypes.array.isRequired,
+};
 
 export default Carousel;
--- src/components/carousel/Carousel.test.js
+++ src/components/carousel/Carousel.test.js
@@ -1,9 +1,116 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import Carousel from './Carousel';
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe('Carousel', () => {
-  test('renders to the DOM', () => {
-    render(<BrowserRouter><Carousel /></BrowserRouter>)
+import { BrowserRouter } from "react-router-dom";
+import Carousel from "./Carousel";
+
+describe("Carousel", () => {
+  test("renders to the DOM", () => {
+    render(
+      <BrowserRouter>
+        <Carousel />
+      </BrowserRouter>
+    );
+  });
+
+  test("first image in array is active by default", () => {
+    const photos = [
+      "https://dummyimage.com/200x125.jpg",
+      "https://dummyimage.com/200x126.jpg",
+    ];
+
+    render(
+      <BrowserRouter>
+        <Carousel photos={photos} />
+      </BrowserRouter>
+    );
+
+    const slide = screen.getByAltText("slide 1").closest("div");
+    expect(slide).toHaveClass("active-slide");
+  });
+
+  test("displays next image when right button clicked, eventually circling back to first image", () => {
+    const photos = [
+      "https://dummyimage.com/200x125.jpg",
+      "https://dummyimage.com/200x126.jpg",
+    ];
+
+    render(
+      <BrowserRouter>
+        <Carousel photos={photos} />
+      </BrowserRouter>
+    );
+
+    const rightButton = screen.getByRole("button", { name: ">" });
+
+    // Second image in array is not active by default
+    const inactiveSecondSlide = screen.getByAltText("slide 2").closest("div");
+    expect(inactiveSecondSlide).toHaveClass("inactive-slide");
+
+    // Click right button to get to next image in array
+    userEvent.click(rightButton);
+
+    const activeSecondSlide = screen.getByAltText("slide 2").closest("div");
+    expect(activeSecondSlide).toHaveClass("active-slide");
+
+    // Click right button to get back to the first image in array
+    userEvent.click(rightButton);
+
+    const activeFirstSlide = screen.getByAltText("slide 1").closest("div");
+    expect(activeFirstSlide).toHaveClass("active-slide");
+  });
+
+  test("displays previous image when left button clicked, displaying last img when pressed on first image", () => {
+    const photos = [
+      "https://dummyimage.com/200x125.jpg",
+      "https://dummyimage.com/200x126.jpg",
+    ];
+
+    render(
+      <BrowserRouter>
+        <Carousel photos={photos} />
+      </BrowserRouter>
+    );
+
+    const leftButton = screen.getByRole("button", { name: "<" });
+
+    // Second image in array is not active by default
+    const inactiveSecondSlide = screen.getByAltText("slide 2").closest("div");
+    expect(inactiveSecondSlide).toHaveClass("inactive-slide");
+
+    // Click left button to get to previous image in array
+    userEvent.click(leftButton);
+
+    const activeSecondSlide = screen.getByAltText("slide 2").closest("div");
+    expect(activeSecondSlide).toHaveClass("active-slide");
+
+    // Click left button to get back to the first image in array
+    userEvent.click(leftButton);
+
+    const activeFirstSlide = screen.getByAltText("slide 1").closest("div");
+    expect(activeFirstSlide).toHaveClass("active-slide");
+  });
+
+  test("displays default house image when given a bad image url", async () => {
+    // An error is thrown in the component when an invalid image URL
+    // is given, so I am temporarily silencing error output and
+    // restoring it to its original functionality at the end of the test
+    const originalError = console.error;
+    console.error = jest.fn();
+
+    const photos = ["aaaaaaaaaa"];
+    render(
+      <BrowserRouter>
+        <Carousel photos={photos} />
+      </BrowserRouter>
+    );
+
+    await waitFor(() => {
+      const firstImg = screen.getByRole("img");
+      expect(firstImg.src).toContain("default-house");
+    });
+
+    // Restoring original error functionality
+    console.error = originalError;
   });
 });
