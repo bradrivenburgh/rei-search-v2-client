8d005de4050c6c45f594f706e951144ac7dedf87
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-04-21 14:18:53 -0400

    adjusted Login.test.js to reflect addition of Test Account div

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import Login from "./Login";
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './Login';
 
-describe("Login", () => {
-  test("renders to the DOM", () => {
+describe('Login', () => {
+  test('renders to the DOM', () => {
     render(
       <BrowserRouter>
         <Login />
@@ -11,25 +11,25 @@ describe("Login", () => {
     );
   });
 
-  test("contains a heading,'Sign-up'", () => {
+  test("contains a heading,'Login'", () => {
     render(
       <BrowserRouter>
         <Login />
       </BrowserRouter>
     );
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toHaveTextContent("Login");
+    const heading = screen.getByRole('heading', { name: /login/i });
+    expect(heading).toHaveTextContent('Login');
   });
 
-  test("contains a form with required input fields", () => {
+  test('contains a form with required input fields', () => {
     render(
       <BrowserRouter>
         <Login />
       </BrowserRouter>
     );
 
-    const labels = ["User Name:", "Password:"];
+    const labels = ['User Name:', 'Password:'];
 
     labels.forEach((label) => {
       const element = screen.getByLabelText(`${label}`);
@@ -44,19 +44,19 @@ describe("Login", () => {
       </BrowserRouter>
     );
 
-    const submitBtn = screen.getByRole("button", { name: "Submit" });
-    const cancelBtn = screen.getByRole("button", { name: "Cancel" });
+    const submitBtn = screen.getByRole('button', { name: 'Submit' });
+    const cancelBtn = screen.getByRole('button', { name: 'Cancel' });
     expect(submitBtn).toBeInTheDocument();
     expect(cancelBtn).toBeInTheDocument();
   });
 
-  test("checks to make sure Submit button is disabled while required fields are empty", () => {
+  test('checks to make sure Submit button is disabled while required fields are empty', () => {
     render(
       <BrowserRouter>
         <Login />
       </BrowserRouter>
     );
 
-    expect(screen.getByRole("button", { name: "Submit" })).toBeDisabled();
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeDisabled();
   });
 });
